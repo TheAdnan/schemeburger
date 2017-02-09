99f0b2c99edaffee9ecf2f37f6952b759ddee59c
TheAdnan <adnankicin92@gmail.com>
2017-02-09 10:03:15 +0100

    dragan portrait edit

--- scripts/dragan-portrait-effect.scm
+++ scripts/dragan-portrait-effect.scm
@@ -7,12 +7,14 @@
 	(let* (
 		(img image)
 		(lyr layer)
+		(novi-layer (car (gimp-layer-copy lyr 0)))
+		(novi-layer2 (car (gimp-layer-copy lyr 0)))
+
 	)
 	(gimp-context-push)
 	(gimp-image-undo-group-start image)
 	
-	;Duplicate layer and desaturate
-	(define novi-layer (car (gimp-layer-copy lyr 0)))
+	;Duplicate layer and desaturate/multiply merge
     (gimp-item-set-name novi-layer "Novi Layer")
     (gimp-image-insert-layer img novi-layer 0 0)
 	(gimp-layer-set-opacity novi-layer 90)
@@ -20,13 +22,16 @@
 	(gimp-desaturate-full novi-layer 2)
 	(gimp-image-merge-down img novi-layer 0)
 	
-	; (define novi-layer2 (car (gimp-layer-copy lyr 0)))
-    ; (gimp-item-set-name novi-layer2 "Novi Layer 2")
-    ; (gimp-image-insert-layer img novi-layer2 0 0)
-	; (gimp-layer-set-opacity novi-layer2 100)
-	; (gimp-layer-set-mode novi-layer2 3)
-	; (gimp-desaturate-full novi-layer2 2)
-	; (gimp-image-merge-down img novi-layer2 0)
+	;Duplicate layer then invert, blur and overlay
+
+    (gimp-item-set-name novi-layer2 "Novi Layer 2")
+    (gimp-image-insert-layer img novi-layer2 0 0)
+	(gimp-layer-set-opacity novi-layer2 55)
+	(gimp-layer-set-mode novi-layer2 OVERLAY-MODE)
+	(gimp-desaturate-full novi-layer2 DESATURATE-LUMINOSITY)
+	(gimp-invert novi-layer2)
+	(plug-in-gauss-rle RUN-NONINTERACTIVE img novi-layer2 20.0 0 0)
+	(gimp-image-merge-down img novi-layer2 0)
 	 
 	(gimp-image-flatten img)
 	
